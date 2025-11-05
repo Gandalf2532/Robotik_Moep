56d992d4ae35479be1fdb0251148183b428144b1
MOEP-Robotik <Moepdbg@outlook.de>
2025-11-05 16:04:09 +0100

    grobes setup von test und hubmenü

---
+++ FLL_2025_26/Programm1.py
@@ -0,0 +1,20 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis, Color
+from pybricks.tools import wait
+
+HUB = PrimeHub()
+
+HUB.imu.reset_heading(0)
+BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 130)
+#130 anpassen, falls falsch. Dann aber allen mitteilen
+BEWEGUNGSMOTOREN.use_gyro(True)
+BEWEGUNGSMOTOREN.settings(straight_speed=800)
+
+BEWEGUNGSMOTOREN.straight(280)
+BEWEGUNGSMOTOREN.turn(90)
+BEWEGUNGSMOTOREN.straight(500)
+BEWEGUNGSMOTOREN.straight(-500)
+BEWEGUNGSMOTOREN.turn(-90)
+BEWEGUNGSMOTOREN.straight(-280)
---
+++ FLL_2025_26/test.py
@@ -0,0 +1,18 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis, Color
+from pybricks.tools import wait
+
+HUB = PrimeHub()
+
+HUB.imu.reset_heading(0)
+BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 130)
+#130 anpassen, falls falsch. Dann aber allen mitteilen
+BEWEGUNGSMOTOREN.use_gyro(True)
+winkel = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 200, 270, 360,]
+
+for i in winkel:
+    BEWEGUNGSMOTOREN.turn(i)
+    wait(500)
+    BEWEGUNGSMOTOREN.turn(-i)
