1284b8b54643d9cdd4822bd800b30c694c16f152
MOEP-Robotik <Moepdbg@outlook.de>
2025-11-05 15:34:56 +0100

    standardeinrichtung hergestellt, probleme endlich gefixxt

---
+++ FLL_2025_26/Infos.md
@@ -0,0 +1,2 @@
+BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 130)
+BEWEGUNGSMOTOREN.use_gyro(True)
---
+++ FLL_2025_26/Standardconfigurationen.py
@@ -0,0 +1,12 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.robotics import DriveBase
+from pybricks.parameters import Port, Direction, Axis, Color
+from pybricks.tools import wait
+
+HUB = PrimeHub()
+
+HUB.imu.reset_heading(0)
+BEWEGUNGSMOTOREN = DriveBase(Motor(Port.D, Direction.COUNTERCLOCKWISE), Motor(Port.C), 56, 130)
+#130 anpassen, falls falsch. Dann aber allen mitteilen
+BEWEGUNGSMOTOREN.use_gyro(True)
